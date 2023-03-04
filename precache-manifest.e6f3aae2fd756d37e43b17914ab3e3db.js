397a1a2a670d127c063d7fcbf8af26c120e2b4cb
ayay459547 <ayay459547@gmail.com>
2023-03-04 08:02:38 +0800

    deploy

---
+++ precache-manifest.e6f3aae2fd756d37e43b17914ab3e3db.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bd7b6d9676d6f2b8b2",
+    "url": "css/app.be08fc53.css"
+  },
+  {
+    "revision": "9da41cd28d21ae6eeee2",
+    "url": "css/chunk-03c123be.71019ee7.css"
+  },
+  {
+    "revision": "2e87c7bf46bbfa5742b9",
+    "url": "css/chunk-24440af0.bb4acfc0.css"
+  },
+  {
+    "revision": "c03e8520537d90f70d3f",
+    "url": "css/chunk-287f69bd.7164e8ec.css"
+  },
+  {
+    "revision": "fb8183fc3cab202c146c",
+    "url": "css/chunk-3c08dea8.b9262152.css"
+  },
+  {
+    "revision": "ff60c9a01460661f6365",
+    "url": "css/chunk-8f0f4b4a.df78e2a7.css"
+  },
+  {
+    "revision": "0c799d4f3037cb469196",
+    "url": "css/chunk-a0535e8c.a5ee0637.css"
+  },
+  {
+    "revision": "6d085d15b5f7aeeaaabb",
+    "url": "css/chunk-dba9aa0a.a57c3642.css"
+  },
+  {
+    "revision": "6787e28752ece6ed36296e44e374d671",
+    "url": "index.html"
+  },
+  {
+    "revision": "89bd7b6d9676d6f2b8b2",
+    "url": "js/app.76536bcb.js"
+  },
+  {
+    "revision": "9da41cd28d21ae6eeee2",
+    "url": "js/chunk-03c123be.99a6f532.js"
+  },
+  {
+    "revision": "2e87c7bf46bbfa5742b9",
+    "url": "js/chunk-24440af0.bf56b5e7.js"
+  },
+  {
+    "revision": "c03e8520537d90f70d3f",
+    "url": "js/chunk-287f69bd.fc921f7b.js"
+  },
+  {
+    "revision": "fb8183fc3cab202c146c",
+    "url": "js/chunk-3c08dea8.ac323585.js"
+  },
+  {
+    "revision": "ff60c9a01460661f6365",
+    "url": "js/chunk-8f0f4b4a.bf69f2a7.js"
+  },
+  {
+    "revision": "0c799d4f3037cb469196",
+    "url": "js/chunk-a0535e8c.5915abc3.js"
+  },
+  {
+    "revision": "6d085d15b5f7aeeaaabb",
+    "url": "js/chunk-dba9aa0a.99265a3b.js"
+  },
+  {
+    "revision": "3ab7a6dc9aae2fbabb28",
+    "url": "js/chunk-vendors.1df71c93.js"
+  },
+  {
+    "revision": "705c2b15ba3856cefaacf32c6ec05073",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
